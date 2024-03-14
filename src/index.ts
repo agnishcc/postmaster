d2a5614d6899a4ffae4b74d6be879d3ae199a3d1
Agnish Chakraborty <30952790+agnishcc@users.noreply.github.com>
2024-03-14 16:36:24 +0530

    feat(app): adds resend funtions
    adds resend email sending features

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { ResendRepo } from './resend'
 
 const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use('/*', cors())
+app.post('/api/contact/submit', async (c) => {
+  const body = JSON.parse(await c.req.text())
+  console.log(body);
+  const resendRepo = new ResendRepo(c.env?.RESEND_API_KEY as string)
+  await resendRepo.sendContactEmail(body.name, body.email, body.message)
+  return c.json({ message: 'Submitted Sucessfully' }, 200)
 })
+
 
 export default app
---
+++ src/resend.ts
@@ -0,0 +1,47 @@
+import { Resend } from 'resend';
+
+
+export class ResendRepo {
+    private apiKey: string;
+
+    constructor(apiKey: string) {
+        this.apiKey = apiKey;
+    }
+
+    async sendContactEmail(name: string, email: string, content: string) {
+        const resend = new Resend(this.apiKey);
+        const emailBody = this.createTemplate(name, email, content);
+
+        const data = await resend.emails.send({
+            from: 'Agnishcc Website <onboarding@resend.dev>',
+            to: 'agnishc.dev@gmail.com',
+            subject: `New Contact, ${name}`,
+            html: emailBody,
+        });
+
+    }
+
+    private createTemplate(name: string, email: string, content: string) {
+        return `<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>New Form Submission by ${name}</title>
+        </head>
+        <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
+        
+            <div style="max-width: 600px; margin: 0 auto; background-color: #fff; border-radius: 10px; padding: 20px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+                <h2 style="color: #333;">New Form Submission</h2>
+                <hr style="border-top: 1px solid #ccc;">
+        
+                <p><strong>Name:</strong> ${name}</p>
+                <p><strong>Email:</strong> ${email}</p>
+                <p><strong>Content:</strong> ${content}</p>
+        
+                <p style="margin-top: 20px; font-size: 0.8em; color: #888;">This email was generated automatically. Please do not reply.</p>
+            </div>
+        
+        </body>
+        </html>`
+    }
+}
